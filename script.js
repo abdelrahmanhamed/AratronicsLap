de5497ead1d89c3b9f0f68765f51415c37aaf841
abdelrahmanhamed <abdelrahman.hamed@student.guc.edu.eg>
2016-07-31 02:08:49 +0300

    finished all and now changing the colors

--- script.js
+++ script.js
@@ -8,6 +8,39 @@ $(document).ready(function($) {
   /* ------------------------- */
   /* open Form Interactions */
   /* ------------------------- */
+  $('#go1').on('click', function(event) {
+    event.preventDefault();
+
+    $('#pdf1').addClass('pdfafter');
+    $('#go1').addClass('goafter');
+    $('#return1').addClass('returnafter');
+    $('#tooltip1').addClass('tooltipafter');
+  });
+  $('#return1').on('click', function(event) {
+    event.preventDefault();
+
+    $('#pdf1').removeClass('pdfafter');
+    $('#go1').removeClass('goafter');
+    $('#return1').removeClass('returnafter');
+    $('#tooltip1').removeClass('tooltipafter');
+  });
+
+  $('#go2').on('click', function(event) {
+    event.preventDefault();
+
+    $('#pdf2').addClass('pdfafter');
+    $('#go2').addClass('goafter');
+    $('#return2').addClass('returnafter');
+    $('#tooltip2').addClass('tooltipafter');
+  });
+  $('#return2').on('click', function(event) {
+    event.preventDefault();
+
+    $('#pdf2').removeClass('pdfafter');
+    $('#go2').removeClass('goafter');
+    $('#return2').removeClass('returnafter');
+    $('#tooltip2').removeClass('tooltipafter');
+  });
   $('#open').on('click', function(event) {
     event.preventDefault();
 
